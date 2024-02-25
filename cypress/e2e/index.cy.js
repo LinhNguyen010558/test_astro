66cb658d4692929e646b133b606a4ea5c6090d94
Dead_Max <ljnkpro010558@gmail.com>
2024-02-25 17:35:36 +0700

    updated

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,5 +1,5 @@
 it('titles are correct', () => {
-    const page = cy.visit('http://localhost:3000');
+    const page = cy.visit('/');
   
     page.get('title').should('have.text', 'Astro Quickstart')
     page.get('h1').should('have.text', 'Demo CI/CD with GitHub Actions');
