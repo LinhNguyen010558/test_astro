f95d66e048f0b57d9874408a4b30320cbfa13a04
Dead_Max <ljnkpro010558@gmail.com>
2024-02-25 17:48:09 +0700

    test 2

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "cypress";
+import mochawesomeReporterPlugin from 'cypress-mochawesome-reporter/plugin';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypres demo report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
-    baseUrl: "http://localhost:3000/",
-    supportFile: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    // baseUrl: "http://localhost:3000/",
+    // supportFile: false,
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
+
+    setupNodeEvents(on) {
+      return mochawesomeReporterPlugin(on);
     },
   },
 });
